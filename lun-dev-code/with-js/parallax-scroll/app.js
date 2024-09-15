544c515d473ec9de36f23f9ba8476ef14e23f255
DarrelA <darrelaiscoding@gmail.com>
2024-09-15 12:42:32 +0800

    `app.js`: `listBg` & `titleBanner`

---
+++ lun-dev-code/with-js/parallax-scroll/app.js
@@ -0,0 +1,23 @@
+let listBg = document.querySelectorAll('.banner .bg');
+let titleBanner = document.querySelector('.banner h1');
+let listTab = document.querySelectorAll('.tab');
+
+window.addEventListener('scroll', (e) => {
+  // scrollY if the web scrollbar position (pixel)
+  let top = this.scrollY;
+
+  /* 
+  index is the order of class bg(0,1,2 ...)
+  when scrolling the web, the class bg scroll down,
+  the bigger the index, the faster the movement
+  */
+  listBg.forEach((bg, i) => {
+    if (i != 0 && i != 8) {
+      bg.style.transform = `translateY(${(top * i) / 2}px)`;
+    } else if (i == 0) {
+      bg.style.transform = `translateY(${top / 3}px)`;
+    }
+  });
+
+  titleBanner.style.transform = `translateY(${(top * 4) / 2}px)`;
+});
