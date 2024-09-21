f67ffb82bebe64a7cf9d11fb57e8d617a684e353
DarrelA <darrelaiscoding@gmail.com>
2024-09-21 12:01:29 +0800

    `animate-tilt`

--- tailwind-labs/glowing-bg-gradient-effect/tailwind.config.js
+++ tailwind-labs/glowing-bg-gradient-effect/tailwind.config.js
@@ -2,7 +2,16 @@
 module.exports = {
   content: ['./**/*.{html,js}', '!./node_modules/**/*'],
   theme: {
-    extend: {},
+    extend: {
+      animation: { tilt: 'tilt 10s 10s infinite linear' },
+      keyframes: {
+        tilt: {
+          '0%, 50%, 100%': { transform: 'rotate(0deg)' },
+          '25%': { transform: 'rotate(1deg)' },
+          '75%': { transform: 'rotate(-1deg)' },
+        },
+      },
+    },
   },
   plugins: [],
 };
